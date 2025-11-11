e2f4c93606505a89920d2e99d644dcfedd9b08a2
freyde <jeffrey.delgado7@gmail.com>
2025-11-11 15:02:16 +0800

    Hompage Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
                             href="{{ route('student.login') }}"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
                         >
-                            Log in
+                            Student portal
                         </a>
                     @endauth
                 </nav>
@@ -44,7 +44,9 @@
         
 
         @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
+            <div class="h-14.5 hidden lg:block">
+                This Homepage is under construction.
+            </div>
         @endif
     </body>
 </html>
