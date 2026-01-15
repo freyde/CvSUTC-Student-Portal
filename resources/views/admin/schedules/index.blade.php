f6064cb6ac5fe3955155baca2d93e03525e18cb6
freyde <jeffrey.delgado7@gmail.com>
2026-01-15 13:50:55 +0800

    update

--- resources/views/admin/schedules/index.blade.php
+++ resources/views/admin/schedules/index.blade.php
@@ -64,7 +64,7 @@
     </div>
 
     <div class="bg-white rounded-lg shadow overflow-hidden">
-        <table class="min-w-full divide-y divide-gray-200">
+        <table class="table-auto min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
                 <tr>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Schedule Code</th>
