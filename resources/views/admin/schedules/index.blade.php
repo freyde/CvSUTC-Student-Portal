d72b973841c59c9e7e51af706f2a404801b0d72a
freyde <jeffrey.delgado7@gmail.com>
2026-02-18 12:00:37 +0800

    Update

--- resources/views/admin/schedules/index.blade.php
+++ resources/views/admin/schedules/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="max-w-7xl mx-auto">
+<div class="max-w-none mx-auto">
     <div class="flex justify-between items-center mb-6">
         <h1 class="text-2xl font-semibold">Manage Schedules</h1>
         <div class="flex gap-2">
